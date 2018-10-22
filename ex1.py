fb1fd324fabb9a1966bbba20b1afb3604f1f42df
Nouf Saleh <nalturaief@gmail.com>
2018-10-22 21:07:39 +0300

    initial commit

---
+++ ex1.py
@@ -0,0 +1,14 @@
+class Udacian:
+
+    def __init__(self, name, city, enrollment, nanodegree, status):
+        self.name = name
+        self.city = city
+        self.enrollment = enrollment
+        self.nanodegree = nanodegree
+        self.status = status
+
+    def printUdacian(self):
+        return("name:" + self.name + "\ncity: " + self.city + "\nenrollment: " + self.enrollment + "\nnanodegree: " + self.nanodegree + "\nstatus: " + self.status)
+
+udacian = Udacian("Sara", "Khobar", "yes", "Full Stack Developer", "active")
+print(udacian.printUdacian())
