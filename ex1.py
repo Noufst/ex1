796479ee56bf67c437a6325b8d908cead807b38f
Nouf Saleh <nalturaief@gmail.com>
2018-10-22 21:27:40 +0300

    Changing name to Nouf

--- ex1.py
+++ ex1.py
@@ -10,5 +10,5 @@ class Udacian:
     def printUdacian(self):
         return("name:" + self.name + "\ncity: " + self.city + "\nenrollment: " + self.enrollment + "\nnanodegree: " + self.nanodegree + "\nstatus: " + self.status)
 
-udacian = Udacian("Sara", "Khobar", "yes", "Full Stack Developer", "active")
+udacian = Udacian("Nouf", "Khobar", "yes", "Full Stack Developer", "active")
 print(udacian.printUdacian())
